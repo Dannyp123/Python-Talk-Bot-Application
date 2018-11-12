794ff8418f2a54880d8a0187cd3b7796d25eb155
basecamp <cvanderpoel@basecampcodingacademy.org>
2018-11-12 11:55:35 -0600

    I made tiny grammar fixes

--- core_bot.py
+++ core_bot.py
@@ -1,5 +1,7 @@
 def hello(name):
-    return '''hello {}\n My name is PyBot'''.format(name)
+    return '''hello {}\nMy name is PyBot'''.format(name)
+
 
 def cool_bro(wtd):
-    return '''That is cool broski!\n {}is one of my favorite things to do!'''.format(wtd)
+    return '''That is cool broski!\n{} is one of my favorite things to do!'''.format(
+        wtd)
