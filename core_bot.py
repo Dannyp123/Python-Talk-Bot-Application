0604e6b0733c1734f2efe373f5ec33dcecc6125d
Daniel Peterson <dpeterson@basecampcodingacademy.org>
2018-11-12 11:55:47 -0600

    started the random message function

--- core_bot.py
+++ core_bot.py
@@ -1,5 +1,18 @@
+from bot_disk import *
+from random import choice
+
+random_phase = build_random_phrase_list()
+
+
 def hello(name):
     return '''hello {}\n My name is PyBot'''.format(name)
 
+
 def cool_bro(wtd):
-    return '''That is cool broski!\n {}is one of my favorite things to do!'''.format(wtd)
+    return '''That is cool broski!\n {} is one of my favorite things to do!'''.format(
+        wtd)
+
+
+def randomMessage(inputs):
+    if inputs != "bye":
+        print(random_phase)
