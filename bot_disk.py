c12e4ef98787232aea20474624955f071e04d2d7
matt2tech <mlipsey@basecampcodingacademy.org>
2018-11-12 11:28:50 -0600

    Added a function that builds a list from the content of bot.txt file

--- bot_disk.py
+++ bot_disk.py
@@ -2,3 +2,11 @@ def read_txt_file():
     with open('bot.txt', 'r') as file:
         contents = file.read()
     return contents
+
+def build_random_phrase_dict():
+    list = []
+    contents = read_txt_file()
+    for i in contents:
+        list.append(i)
+    return list
+
