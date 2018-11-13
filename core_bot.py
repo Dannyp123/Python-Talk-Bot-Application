658cc652412c251363d45a9b2f620ad51f0e11ec
Daniel Peterson <dpeterson@basecampcodingacademy.org>
2018-11-13 09:53:10 -0600

    Made an age function

--- core_bot.py
+++ core_bot.py
@@ -1,5 +1,6 @@
 from bot_disk import *
 from random import choice, randint
+from time import ctime
 
 
 def hello(name):
@@ -22,3 +23,9 @@ def guessing_game(text):
         return True
     else:
         return False
+
+
+def age(text):
+    if "age" in text:
+        time = ctime()
+        return "I'm {} years old!".format(int(time.split()[-1]) - 2018)
