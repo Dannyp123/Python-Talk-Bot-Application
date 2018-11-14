c93520b1a0daec08287cfd34f9a92386f919d714
matt2tech <mlipsey@basecampcodingacademy.org>
2018-11-14 13:31:27 -0600

    Built a test for get_jokes

--- test_core.py
+++ test_core.py
@@ -3,9 +3,18 @@ from bcca.test import (
     should_print,
     fake_file,
 )
+from unittest import mock
 
 def test_phrase_dict():
     list = phrase_dict()
     list2 = phrase_dict()
 
     assert list == list2
+
+@mock.patch('core_bot.randint')
+def test_get_jokes_1(fake_num):
+    fake_num.side_effect = [7]
+
+    joke = get_jokes()
+
+    assert joke == 'What do you call a pig that knows karate. Pork chop!'
