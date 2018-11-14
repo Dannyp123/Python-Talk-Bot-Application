89a025374c29dd24f380c01c0d7c9c0030243823
matt2tech <mlipsey@basecampcodingacademy.org>
2018-11-14 13:07:00 -0600

    Built a test for append_phrases

--- test_disk.py
+++ test_disk.py
@@ -31,3 +31,15 @@ def test_build_random_phrase_list_1():
 @fake_file({'bot.txt': ''})
 def test_build_random_phrase_list_2():
     assert build_random_phrase_list() == ['']
+
+@fake_file({'bot.txt': '''line1
+line2
+line3
+'''})
+def test_append_phrases_1():
+    phrase = 'line4'
+    phrase2 = 'line2'
+    append_phrases(phrase)
+    append_phrases(phrase2)
+
+    assert open('bot.txt').read() == 'line1\nline2\nline3\nline4\n'
