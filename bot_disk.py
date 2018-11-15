2c819f1ceca7dd33855b7fd522b18b310556467d
basecamp <cvanderpoel@basecampcodingacademy.org>
2018-11-15 10:07:26 -0600

    I implemented the censorship method from the profanity library

--- bot_disk.py
+++ bot_disk.py
@@ -1,17 +1,22 @@
+from profanity import profanity
+
+
 def read_txt_file():
     with open('bot.txt', 'r') as file:
         contents = file.read()
     return contents.strip()
 
+
 def build_random_phrase_list():
-    list = []
+    l = []
     contents = read_txt_file().split('\n')
     for i in contents:
-        list.append(i)
-    return list
+        l.append(i)
+    return l
+
 
 def append_phrases(phrase):
-    list = build_random_phrase_list()
-    if phrase not in list:
+    phrases = build_random_phrase_list()
+    if phrase not in phrases:
         with open('bot.txt', 'a') as file:
-            file.write(phrase + "\n")
+            file.write(profanity.censor(phrase) + "\n")
